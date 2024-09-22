070166c4f29eb7f610a2439bb913fdc1b5a2a657
Claudio <fukudesignandco@gmail.com>
2024-09-22 15:06:02 -0300

    protegemos las acciones con Devise y configuramos las vistas basicas

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was successfully added.'
+    else
+      redirect_to post_path(@post), alert: 'Unable to add comment.'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@comment.post), notice: 'Comment was successfully deleted.'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
