314759af90ab483c437a2bdccec9773282c80836
Claudio <fukudesignandco@gmail.com>
2024-09-22 15:01:34 -0300

    Configuramos las rutas y controladores

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'posts#index'
+  
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
 end
